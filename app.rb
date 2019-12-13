c7f7c4a989c89ec3528135d245beb4cefcf50dc9
Franz Vilela Campero <franz_cv456@hotmail.com>
2019-12-13 08:48:36 -0400

    hot fixes

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
+require './lib/Tablero'
 
 $cantidad_de_autos = 1
-
 
 get '/' do
     @cantidad_de_autos = $cantidad_de_autos
@@ -9,9 +9,18 @@ get '/' do
 end
 
 post '/tablero' do
-    @cantAutos=params[:cantAutos]
+    @tablero = Tablero.new
+    @cantAutos = $cantidad_de_autos.to_i
     @size=params[:sizes].to_i
-
+    for i in 1..@cantAutos do
+        tmp_orientacion = 'orientacion' + i.to_s
+        tmp_pos_x = 'posx' + i.to_s
+        tmp_pos_y = 'posy' + i.to_s
+        @posX = params[:tmp_pos_x]
+        @posY = params[:tmp_pos_y]
+        @orientacion = params[:tmp_orientacion]
+        @tablero.addAuto(@posX, @posY, @orientacion)
+    end
     erb :tablero
 end
 
